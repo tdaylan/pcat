47cc037461258522d519fde6f439b2171be24673
Tansu Daylan <tansu.daylan@gmail.com>
2016-04-30 21:11:43 -0400

    NA

---
+++ py/__init__.py
@@ -0,0 +1,49 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+# plotting
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+mpl.rc('image', interpolation='none', origin='lower')
+
+import seaborn as sns
+sns.set(context='poster', style='ticks', color_codes=True)
+
+# numpy
+import numpy as np
+from numpy import *
+from numpy.random import *
+from numpy.random import choice
+
+# scipy
+import scipy as sp
+from scipy import ndimage
+from scipy.interpolate import *
+from scipy.special import erfinv, erf
+from scipy.stats import poisson as pss
+from scipy import ndimage
+
+# multiprocessing
+import multiprocessing as mp
+
+# healpy
+import healpy as hp
+from healpy.rotator import angdist
+from healpy import ang2pix
+
+# pyfits
+import pyfits as pf
+
+# utilities
+import os, time, sys, datetime, warnings, getpass, glob, fnmatch
+
+# tdpy
+import pnts_tran
+import tdpy_util
+
+#listmodl = glob.glob(os.path.dirname(__file__) + "/*.py")
+#__all__ = [os.path.basename(modl)[:-3] for modl in listmodl if os.path.isfile(modl)]
+
