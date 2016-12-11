7ea2b464dfb24cbb060726e61cad573baed16ca8
Tansu Daylan <tansu.daylan@gmail.com>
2016-12-11 17:34:40 -0500

    NA

--- gcol.py
+++ gcol.py
@@ -1,23 +1,23 @@
 from __init__ import *
 
-def gcol():
-    os.system('rm -rf $PCAT_DATA_PATH/imag/*_50')
-    os.system('rm -rf $PCAT_DATA_PATH/imag/*_5')
-    os.system('rm -rf $PCAT_DATA_PATH/imag/*_100')
-    os.system('rm -rf $PCAT_DATA_PATH/imag/*_1000')
-   
-    for strgextn in ['/imag/', '/data/outp/']:
-        
-        path = os.environ["PCAT_DATA_PATH"] + strgextn
+os.system('rm -rf $PCAT_DATA_PATH/imag/*_50')
+os.system('rm -rf $PCAT_DATA_PATH/imag/*_5')
+os.system('rm -rf $PCAT_DATA_PATH/imag/*_100')
+os.system('rm -rf $PCAT_DATA_PATH/imag/*_1000')
 
-        for strgfile in os.listdir(path):
-            pathfile = path + strgfile
-            if os.path.isdir(pathfile) and strgfile[:8].isdigit():
-                
+for strgextn in ['/imag/', '/data/outp/']:
+    
+    path = os.environ["PCAT_DATA_PATH"] + strgextn
+
+    for strgfile in os.listdir(path):
+        pathfile = path + strgfile
+        if os.path.isdir(pathfile) and strgfile[:8].isdigit():
+            
+            try:
                 numb = int(pathfile[pathfile.rfind('_')+1:])
-                
+            
                 if not os.path.exists(pathfile + '/anim') or numb < 100000:
                     os.system('rm -rf ' + pathfile)
-
-globals().get(sys.argv[1])()
+            except:
+                pass
 
