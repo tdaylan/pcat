0f458a03a9845824e82b12f754f0d0057020bdbf
Tansu Daylan <tansu.daylan@gmail.com>
2017-04-16 00:08:48 -0400

    NA

--- gcol.py
+++ gcol.py
@@ -3,18 +3,22 @@ from __init__ import *
 pathchec = os.environ["PCAT_DATA_PATH"] + '/data/outp/comp.txt'
 
 for strgextn in ['/imag/', '/data/outp/']:
-    
+
     path = os.environ["PCAT_DATA_PATH"] + strgextn
 
     for strgfile in os.listdir(path):
         pathfile = path + strgfile
         if os.path.isdir(pathfile) and strgfile[:8].isdigit():
-            
+
+            pathchec = pathfile.replace('imag', 'data/outp') + '/comp.txt'
+
             try:
                 numbswep = int(pathfile[pathfile.rfind('_')+1:])
-            
                 if not os.path.exists(pathchec) or numbswep < 100000:
                     os.system('rm -rf ' + pathfile)
+                    print 'Deleting %s' % pathchec
+                else:
+                    print 'Saving %s' % pathchec
             except:
                 pass
 
