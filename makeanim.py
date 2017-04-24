19eac8b3c5f73f8944c16a062bf8cacfce6c8914
Tansu Daylan <tansu.daylan@gmail.com>
2017-04-24 09:42:29 -0400

    NA

--- makeanim.py
+++ makeanim.py
@@ -12,57 +12,61 @@ for pathruns in listpathruns:
             pathframextn = pathimag + pathruns + '/' + namesampdist + '/fram/' + nameextn
             pathanimextn = pathimag + pathruns + '/' + namesampdist + '/anim/' + nameextn
         
-            print 'pathframextn'
-            print pathframextn
-
             try:
                 listfile = fnmatch.filter(os.listdir(pathframextn), '*_swep*.pdf')
-                listfiletemp = []
-                for thisfile in listfile:
-                    listfiletemp.extend((thisfile.split('_')[0]).rsplit('/', 1))
-                
-                listname = list(set(listfiletemp))
-    
-                for name in listname:
-                    
-                    strgtemp = '%s*_swep*.pdf' % name
-                    listfile = fnmatch.filter(os.listdir(pathframextn), strgtemp)
-                    numbfile = len(listfile)
-                    liststrgextn = []
-                    for k in range(numbfile):
-                        liststrgextn.append((listfile[k].split(name)[1]).split('_')[0])
-                    
-                    liststrgextn = list(set(liststrgextn))
-                    
-                    for k in range(len(liststrgextn)):
-                
-                        listfile = fnmatch.filter(os.listdir(pathframextn), name + liststrgextn[k] + '_swep*.pdf')
-                        numbfile = len(listfile)
-                        
-                        indxfilelowr = 0
-                        
-                        if indxfilelowr < numbfile:
-                            indxfileanim = arange(indxfilelowr, numbfile)
-                        else:
-                            continue
-                            
-                        indxfileanim = choice(indxfileanim, replace=False, size=indxfileanim.size)
-                        
-                        cmnd = 'convert -delay 20 -density 300 -quality 100 '
-                        for n in range(indxfileanim.size):
-                            cmnd += '%s%s ' % (pathframextn, listfile[indxfileanim[n]])
-
-                        namegiff = '%s%s.gif' % (pathanimextn, name + liststrgextn[k])
-                        cmnd += ' ' + namegiff
-                        if not os.path.exists(namegiff):
-                            print 'Run: %s, pdf: ' % (pathruns, namesampdist)
-                            print 'Making %s animation...' % name
-                            print
-                            os.system(cmnd)
-                        else:
-                            pass
-                            #print '%s already exists.' % namegiff
             except:
-                pass
+                print '%s failed.' % pathframextn
+                continue
+
+            listfiletemp = []
+            for thisfile in listfile:
+                listfiletemp.extend((thisfile.split('_')[0]).rsplit('/', 1))
+            
+            listname = list(set(listfiletemp))
+    
+            for name in listname:
+                
+                if not name.startswith('datacnts'):
+                    continue
+
+                strgtemp = '%s*_swep*.pdf' % name
+                listfile = fnmatch.filter(os.listdir(pathframextn), strgtemp)
+                numbfile = len(listfile)
+                liststrgextn = []
+                for k in range(numbfile):
+                    liststrgextn.append((listfile[k].split(name)[1]).split('_')[0])
+                
+                liststrgextn = list(set(liststrgextn))
+                
+                for k in range(len(liststrgextn)):
+            
+                    listfile = fnmatch.filter(os.listdir(pathframextn), name + liststrgextn[k] + '_swep*.pdf')
+                    numbfile = len(listfile)
+                    
+                    indxfilelowr = 0
+                    
+                    if indxfilelowr < numbfile:
+                        indxfileanim = arange(indxfilelowr, numbfile)
+                    else:
+                        continue
+                        
+                    indxfileanim = choice(indxfileanim, replace=False, size=indxfileanim.size)
+                    
+                    cmnd = 'convert -delay 20 -density 300 -quality 100 '
+                    for n in range(indxfileanim.size):
+                        cmnd += '%s%s ' % (pathframextn, listfile[indxfileanim[n]])
+
+                    namegiff = '%s%s.gif' % (pathanimextn, name + liststrgextn[k])
+                    cmnd += ' ' + namegiff
+                    if not os.path.exists(namegiff):
+                        print 'Run: %s, pdf: %s' % (pathruns, namesampdist)
+                        print 'Making %s animation...' % name
+                        print
+                        os.system(cmnd)
+                    else:
+                        pass
+                        #print '%s already exists.' % namegiff
+            #except:
+            #    pass
                 #print 'failed for %s' % pathframextn
 
--- merg.py
+++ merg.py
@@ -1,9 +1,7 @@
 from __init__ import *
-from PyPDF2 import PdfFileMerger
-
-pdfs = ['file1.pdf', 'file2.pdf', 'file3.pdf', 'file4.pdf']
 
 nameplot = 'gmrbmaps_00'
+
 pathimag = os.environ["PCAT_DATA_PATH"] + '/imag/'
 listpathruns = fnmatch.filter(os.listdir(pathimag), '20*')
 numbruns = len(listpathruns)
@@ -17,16 +15,9 @@ for k in range(numbruns):
         listrtag.append(listpathruns[k])
         listpath.append(pathgmrb)
 
-merger = PdfFileMerger()
-
 cmnd = 'mkdir -p ' + pathimag + nameplot
-print cmnd
 os.system(cmnd)
 for path, rtag, namesampdist in zip(listpath, listrtag, listnamesampdist):
     cmnd = 'cp ' + path + ' ' + pathimag + nameplot + '/' + rtag + '_' + namesampdist + '.pdf'
-    print cmnd
     os.system(cmnd)
-    #if os.path.exists(path):
-    #    merger.append(path)
-#merger.write(pathimag + nameplot)
 
