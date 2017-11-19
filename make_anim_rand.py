863e84cb79f2a67421bf906e2d4510b9cea44334
Tansu Daylan <tansu.daylan@gmail.com>
2017-11-19 17:56:48 -0500

    NA

--- make_anim_rand.py
+++ make_anim_rand.py
@@ -4,7 +4,7 @@ from util import *
 rtag = sys.argv[1]
 strgplot = sys.argv[2]
 pathimag = os.environ["PCAT_DATA_PATH"] + '/imag/%s/post/fram/' % rtag
-listfram = fnmatch.filter(os.listdir(pathimag), '*%s*.pdf' % strgplot)
+listpathfram = fnmatch.filter(os.listdir(pathimag), '*%s*.pdf' % strgplot)
 cmnd = 'convert '
 numbfram = len(listfram)
 indxfram = arange(numbfram)
@@ -12,6 +12,6 @@ indxframrand = choice(indxfram, size=numbfram, replace=False)
 for fram in listpathfram[indxframrand]:
     cmnd += '%s ' % fram
 cmnd += '%s.gif' % strgplot
-#os.system(cmnd)
 print cmnd
+os.system(cmnd)
 
