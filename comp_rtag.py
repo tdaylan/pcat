a4f03c29a9ab9f4369aca04834478453d5147a51
Tansu Daylan <tansu.daylan@gmail.com>
2018-01-30 15:21:50 -0500

    NA

--- comp_rtag.py
+++ comp_rtag.py
@@ -11,18 +11,13 @@ def comp(nameplot):
     
     cmnd = 'mkdir -p ' + pathimag + 'comprtag/' + nameplot + '/'
     print cmnd
-    #os.system(cmnd)
+    os.system(cmnd)
     
-    cmnd = 'mkdir -p ' + pathimag + 'compgold/' + nameplot + '/'
-    print cmnd
-    #os.system(cmnd)
-    
+    print 'listline'
+    print listline
     for line in listline:
         
-        if line in listlinegold:
-            strgtemp = 'gold'
-        else:
-            strgtemp = 'rtag'
+        strgtemp = 'rtag'
         namedest = pathimag + 'comp' + strgtemp + '/' + nameplot + '/' + line + '.pdf'
         if not os.path.isfile(namedest):
             if boolfink:
@@ -31,16 +26,11 @@ def comp(nameplot):
                 cmnd = 'cp %s/' % pathimag + line + '/' + nameplot + '.pdf ' + namedest 
             
             print cmnd
-            #os.system(cmnd)
+            print
+            os.system(cmnd)
 
 
 print 'comp_rtag() initialized...'
-
-# list of golden runs
-listlinegold = [ \
-                '20180125_222826_pcat_chan_inpt_home7msc06000000_10000', \
-                '20180125_223105_pcat_chan_inpt_home7msc06000001_10000', \
-               ]
 
 if os.uname()[1] == 'fink1.rc.fas.harvard.edu' or os.uname()[1] == 'fink2.rc.fas.harvard.edu':
     boolfink = True
@@ -48,7 +38,6 @@ else:
     boolfink = False
 
 pathimag = os.environ["PCAT_DATA_PATH"] + '/imag/'
-pathgold = pathimag + 'compgold/'
 
 print 'Listing the available runs...'
 if boolfink:
@@ -62,12 +51,7 @@ if boolfink:
     print 'Copying the list to the local directory...'
     cmnd = 'scp tansu@fink2.rc.fas.harvard.edu:/n/fink1/tansu/data/pcat/imag/listfink.txt %s' % pathimag
     print cmnd
-    #os.system(cmnd)
-
-print 'Erasing the gold compilation contents...'
-cmnd = 'rm -rf ' + pathgold
-print cmnd
-os.system(cmnd)
+    os.system(cmnd)
 
 if boolfink:
     pathlist = pathimag + 'listfink.txt'
@@ -77,7 +61,7 @@ else:
 with open(pathlist) as thisfile:
     listline = thisfile.readlines()
     listline = [x.strip() for x in listline] 
-strgsrch = '20*chan_inpt*'
+strgsrch = '20180129_*_test_info_*_100000'
 listline = fnmatch.filter(listline, strgsrch)
 
 print 'listline'
@@ -92,7 +76,8 @@ if boolfink:
         print 'Copying reference run to the local directory...'
         cmnd = 'scp -r tansu@fink2.rc.fas.harvard.edu:/n/fink1/tansu/data/pcat/imag/%s/ %s' % (namerefr, pathimag)
         print cmnd
-        #os.system(cmnd)
+        os.system(cmnd)
+        print
 
 print 'Iterating over folders of the reference run...'
 for namefrst in os.listdir(pathrefr):
