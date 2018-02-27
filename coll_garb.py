6bf679564eae4e0ab822aaed854bf86b694461b6
Tansu Daylan <tansu.daylan@gmail.com>
2018-02-27 14:21:35 -0500

    NA

--- coll_garb.py
+++ coll_garb.py
@@ -33,8 +33,7 @@ for strgextn in liststrgextn:
             strgtemp = pathfile[pathfile.rfind('_')+1:]
             if strgtemp.endswith('tile'):
                 strgtemp = strgtemp[:-4]
-            numbswep = int(strgtemp)
-            if ((not os.path.isfile(pathchec) or not boolkeep or numbswep <= 1000) and not 'mockonly' in rtag) or boolforcdele:
+            if strgtemp.isdigit() and ((not os.path.isfile(pathchec) or not boolkeep or int(strgtemp) <= 1000) and not 'mockonly' in rtag) or boolforcdele:
                 print 'Deleting %s...' % pathchec
                 cmnd = 'rm -rf ' + pathfile
                 os.system(cmnd)
