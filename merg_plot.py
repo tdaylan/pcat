7c4b12702412fb65a2fd9c4c0d7fceb0666145bc
Tansu Daylan <tansu.daylan@gmail.com>
2018-03-15 12:03:34 -0400

    NA

--- merg_plot.py
+++ merg_plot.py
@@ -3,6 +3,12 @@ from util import *
 
 print 'PCAT plot merging routine'
 
+if len(sys.argv) > 2:
+    listrtag = sys.argv[1:]
+else:
+    strgsrch = sys.argv[1]
+    listrtag = fnmatch.filter(listline, strgsrch)
+
 pathimag = os.environ["PCAT_DATA_PATH"] + '/imag/'
 listrtag = fnmatch.filter(os.listdir(pathimag), '20*_pcat_*')
 listgdat = retr_listgdat(listrtag, typegdat='init')
